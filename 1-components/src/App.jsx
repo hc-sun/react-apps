9e93442a5cb1340347cfbf6ba6855e84a73b5328
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-12-02 17:19:46 +0800

    added random word

--- 1-components/src/App.jsx
+++ 1-components/src/App.jsx
@@ -1,10 +1,20 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
+  const randomWord = reactDescriptions[genRandomInt(3)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {randomWord} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
