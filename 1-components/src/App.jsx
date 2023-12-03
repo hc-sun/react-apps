3900185c62f8cf7acd40ed0862ededa1439911ee
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-12-03 18:04:13 +0800

    moved components to files

--- 1-components/src/App.jsx
+++ 1-components/src/App.jsx
@@ -1,47 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function Header() {
-  const randomWord = reactDescriptions[genRandomInt(3)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {randomWord} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// function CoreConcept(props) {
-//   return (
-//     <li>
-//       <img src={props.image} alt={props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-//     </li>
-//   );
-// }
-
-// object destructuring
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
