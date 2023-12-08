0e69620d6bac16426da49a247124338f473ba193
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-12-08 17:38:48 +0800

    added edit player name

--- 2-tic-tac-toe-game/src/components/Player.jsx
+++ 2-tic-tac-toe-game/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
-    return (
-        <li>
-        <span className="player">
-            <span className="player-name">{name}</span>
-            <span className="player-symbol">{symbol}</span>
-        </span>
-        <button>Edit</button>
-        </li>
-    );
+  const [editing, setEditing] = useState(false);
+
+  function handleEdit() {
+    setEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (editing) {
+    playerName = <input type="text" required />;
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>Edit</button>
+    </li>
+  );
 }
