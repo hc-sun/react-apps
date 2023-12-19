6fbe3dbc3cb4d990b150f1602e22cd8c1592062d
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-12-19 17:22:09 +0800

    added useState for UserInput

--- 3-investment-calculator/src/components/UserInput.jsx
+++ 3-investment-calculator/src/components/UserInput.jsx
@@ -1,25 +1,67 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1000,
+    expectedReturn: 2,
+    duration: 10,
+  });
+
+  function handleChange(inputType, newValue) {
+    setUserInput((prevState) => {
+      // only update the value that has changed based on inputType
+      return { ...prevState, [inputType]: newValue };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
         </p>
       </div>
 
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleChange("duration", event.target.value)}
+          />
         </p>
       </div>
     </section>
