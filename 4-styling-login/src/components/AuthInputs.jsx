054103632cc04453923e86cfd75551e9e94a4ba0
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-12-22 15:28:19 +0800

    added conditional styled component

--- 4-styling-login/src/components/AuthInputs.jsx
+++ 4-styling-login/src/components/AuthInputs.jsx
@@ -15,16 +15,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${(props) => (props.$invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${(props) => (props.$invalid ? "#ef4444" : "#374151F")};
+  border: 1px solid ${(props) => (props.$invalid ? "#f73f3f" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
@@ -53,10 +53,9 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ContainerDiv>
         <p>
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </Label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
+            $invalid={emailNotValid}
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? 'salmon' : 'transparent',
@@ -66,12 +65,10 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Password
-          </Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
