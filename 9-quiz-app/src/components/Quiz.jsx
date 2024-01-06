5885e0ba0f8e07c9af75422ceb5e87c99d550fc9
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-06 22:46:45 +0800

    implemented handling of user answers

--- 9-quiz-app/src/components/Quiz.jsx
+++ 9-quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIdx = userAnswers.length;
+
+  const quizIsOver = activeQuestionIdx === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
@@ -13,12 +16,25 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsOver) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="quiz complete" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIdx].answers].sort(
+    () => Math.random() - 0.5
+  );
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIdx].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIdx].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
