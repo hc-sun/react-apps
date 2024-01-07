5df12bd5df20d9860db3b9861b859e6d38d99655
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-07 18:51:33 +0800

    added key prop to recreate QuestionTimer component

--- 9-quiz-app/src/components/QuestionTimer.jsx
+++ 9-quiz-app/src/components/QuestionTimer.jsx
@@ -4,13 +4,17 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [timeLeft, setTimeLeft] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => clearTimeout(timer);
   }, [onTimeout, timeout]);
 
   useEffect(() => {
-    setInterval(() => {
+    const inverval = setInterval(() => {
       setTimeLeft((prevTimeLeft) => prevTimeLeft - 100);
     }, 100);
+
+    return () => clearInterval(inverval);
   }, []);
 
   return <progress id="question-time" max={timeout} value={timeLeft} />;
