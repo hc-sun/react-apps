c9bb743e02ff68c37d15c8a091e288cb97467d14
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-07 21:00:36 +0800

    added answerState and css for selected answer

--- 9-quiz-app/src/components/Quiz.jsx
+++ 9-quiz-app/src/components/Quiz.jsx
@@ -6,19 +6,37 @@ import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
+  const [answerState, setAnswerState] = useState("");
 
-  const activeQuestionIdx = userAnswers.length;
+  // if haven't answered current question, activeQuestionIdx is the length of the array
+  const activeQuestionIdx =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
   const quizIsOver = activeQuestionIdx === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
-    setUserAnswers((prevAnswers) => {
-      return [...prevAnswers, selectedAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((prevAnswers) => {
+        return [...prevAnswers, selectedAnswer];
+      });
+    },
+
+    setTimeout(() => {
+      // first answer is the correct one
+      if (selectedAnswer === QUESTIONS[activeQuestionIdx].answers[0]) {
+        setAnswerState("correct");
+      } else {
+        setAnswerState("wrong");
+      }
+
+      setTimeout(() => {
+        // after marking answer, reset state
+        setAnswerState("");
+      }, 2000);
+    }, 1000),
+    [activeQuestionIdx]
+  );
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -48,13 +66,36 @@ export default function Quiz() {
         />
         <h2>{QUESTIONS[activeQuestionIdx].text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswers.map((answer) => {
+            // check if current output "answer" is user's answer
+            const isSeletecdAnswer =
+              userAnswers[userAnswers.length - 1] === answer;
+            let cssClass = "";
+
+            // if current answer is seletecd answer, add css to cssClass
+            if (answerState === "answered" && isSeletecdAnswer) {
+              cssClass = "selected";
+            }
+
+            // add css to cssClass if current answer is correct or wrong
+            if (
+              answerState === "correct" ||
+              (answerState === "wrong" && isSeletecdAnswer)
+            ) {
+              cssClass = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
