925612d7a6245610299ab575b61e3dd8ac5dd542
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-09 16:43:30 +0800

    added stats in Summary

--- 9-quiz-app/src/components/Summary.jsx
+++ 9-quiz-app/src/components/Summary.jsx
@@ -2,22 +2,36 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions.js";
 
 export default function Summary({ userAnswers }) {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+
+  const skippedAnswerPercentage = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswerPercentage = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswerPercentage =
+    100 - correctAnswerPercentage - skippedAnswerPercentage;
+
   return (
     <div id="summary">
       <img src={quizCompleteImg} alt="quiz complete" />
       <h2>Quiz Completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswerPercentage}%</span>
           <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswerPercentage}%</span>
           <span className="text">answered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
-          <span className="text">answered correctly</span>
+          <span className="number">{wrongAnswerPercentage}%</span>
+          <span className="text">answered incorrectly</span>
         </p>
       </div>
       <ol>
@@ -33,7 +47,7 @@ export default function Summary({ userAnswers }) {
           }
 
           return (
-            <li key={answer}>
+            <li key={index}>
               <h3>{index + 1}</h3>
               <p className="question">{QUESTIONS[index].text}</p>
               <p className={cssClass}>{answer ?? "Skipped"}</p>
