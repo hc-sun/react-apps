7453e18f650d202b30648627279b92d2db6d1c5b
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-12 16:55:05 +0800

    added Error component

--- 8-place-picker-app-http-request/src/components/AvailablePlaces.jsx
+++ 8-place-picker-app-http-request/src/components/AvailablePlaces.jsx
@@ -1,26 +1,43 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({
   onSelectPlace,
   isLoading,
   loadingText,
 }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false); // loading state
+  const [availablePlaces, setAvailablePlaces] = useState([]); // data state
+  const [error, setError] = useState(); // error state
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const data = await response.json();
-      setAvailablePlaces(data.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Could not fetch places.");
+        }
+
+        setAvailablePlaces(data.places);
+      } catch (error) {
+        setError({ message: error.message || "Could not fetch places." });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="Something went wrong" message={error.message} />;
+  }
 
   return (
     <Places
