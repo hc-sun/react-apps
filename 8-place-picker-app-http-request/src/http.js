5b83c57af403015887c1a387f22a89336a920001
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-13 16:08:44 +0800

    added PUT request to update user places

--- 8-place-picker-app-http-request/src/http.js
+++ 8-place-picker-app-http-request/src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return data.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Could not update user places.");
+  }
+
+  return data.message;
+}
