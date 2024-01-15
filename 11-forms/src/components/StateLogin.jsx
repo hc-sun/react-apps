e765ac810719eed7639623baa54204b54b5c95af
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-15 18:43:04 +0800

    manage user input via state

---
+++ 11-forms/src/components/StateLogin.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export default function Login() {
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [enteredData, setEnteredData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredData);
+  }
+
+  // function handleEmailChange(event) {
+  //   setEmail(event.target.value);
+  // }
+  function handleInputChange(identifier, value) {
+    setEnteredData((prevState) => ({
+      ...prevState,
+      [identifier]: value,
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredData.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredData.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
