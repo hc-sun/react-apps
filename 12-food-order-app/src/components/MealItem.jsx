a58c93dae516fbd67963098b7d10b57ab05a6cab
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-21 20:50:18 +0800

    use CartContext to count item amount

--- 12-food-order-app/src/components/MealItem.jsx
+++ 12-food-order-app/src/components/MealItem.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function hanleAddToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -14,7 +23,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={hanleAddToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
