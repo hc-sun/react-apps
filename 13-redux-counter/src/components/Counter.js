e33108b977ac86b897d309581b37192d0e022cf5
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-28 19:17:09 +0800

    migrated to redux toolkit

--- 13-redux-counter/src/components/Counter.js
+++ 13-redux-counter/src/components/Counter.js
@@ -1,26 +1,28 @@
 import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
+  // extract data (counter property) from the store state
   const counter = useSelector((state) => state.counter);
   const showCounter = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggle());
   };
 
   return (
