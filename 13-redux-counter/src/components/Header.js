9b2221d1c6fb0bda235d7a334f6552d771644735
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-01-30 21:38:22 +0800

    added dispatch logout action

--- 13-redux-counter/src/components/Header.js
+++ 13-redux-counter/src/components/Header.js
@@ -1,9 +1,15 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Header.module.css";
+import { authActions } from "../store/index";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
 
   return (
     <header className={classes.header}>
@@ -18,7 +24,7 @@ const Header = () => {
               <a href="/">My Sales</a>
             </li>
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           </ul>
         </nav>
