9b1a36a80bd592f63f64dda9179704974d76978a
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-02 17:32:57 +0800

    added cartQuantity

--- 14-redux-cart/src/store/cart-slice.js
+++ 14-redux-cart/src/store/cart-slice.js
@@ -7,6 +7,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       // if not find, false, add new item
       if (!existingItem) {
         state.items.push({
@@ -25,6 +26,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         // remove item
         state.items = state.items.filter((item) => item.id !== id);
