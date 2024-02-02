8f11478736c2901fcc7a1ecd768d47d69a63b35e
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-02 17:32:06 +0800

    added dummy products

--- 14-redux-cart/src/components/Shop/Products.js
+++ 14-redux-cart/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Product 1",
+    description: "This is the first product - amazing!",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Product 2",
+    description: "This is the second product - good!",
+  },
+  {
+    id: "p3",
+    price: 7,
+    title: "Product 3",
+    description: "This is the third product - nice!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
