4d7f076b2ee0e2ba63e5dd7d70e243a978571b52
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-08 20:31:51 +0800

    added products route

--- 15-react-router/src/App.js
+++ 15-react-router/src/App.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
+import Products from "./components/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
