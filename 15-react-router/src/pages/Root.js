8cb41ea1be6707e38e4901cd1577caa3a121af56
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-13 17:08:17 +0800

    added styling to RootLayout

--- 15-react-router/src/pages/Root.js
+++ 15-react-router/src/pages/Root.js
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
 
 function RootLayout() {
   return (
     <>
-      <h1>Root Layout</h1>
-      <Outlet />
+      <MainNavigation />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 }
