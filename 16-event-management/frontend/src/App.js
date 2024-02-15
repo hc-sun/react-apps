d98976291901738328b3fbeb42b62a0a96d8eaca
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-15 21:35:33 +0800

    added route definitions for pages

--- 16-event-management/frontend/src/App.js
+++ 16-event-management/frontend/src/App.js
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import EventDetail from "./pages/EventDetail";
+import NewEvent from "./pages/NewEvent";
+import EditEvent from "./pages/EditEvent";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/events", element: <Events /> },
+  { path: "/events/:eventId", element: <EventDetail /> },
+  { path: "/events/new", element: <NewEvent /> },
+  { path: "/events/:eventId/edit", element: <EditEvent /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
