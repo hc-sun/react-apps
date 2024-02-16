9133efed9ff4b90089ea45263417014f04280b1b
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-16 12:45:07 +0800

    added DUMMY_EVENTS

--- 16-event-management/frontend/src/pages/Events.js
+++ 16-event-management/frontend/src/pages/Events.js
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Programming for everyone",
+  },
+  {
+    id: "e2",
+    title: "Networking for introverts",
+  },
+];
+
 function EventsPage() {
-  return <h1>Events Page</h1>;
+  return (
+    <>
+      <h1>All Events</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={`/events/${event.id}`}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
