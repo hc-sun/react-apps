dd46e5ea33a57378444b8b147bc1bb1219614ba1
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-18 21:04:41 +0800

    implemented error handling

--- 16-event-management/frontend/src/pages/Events.js
+++ 16-event-management/frontend/src/pages/Events.js
@@ -4,9 +4,14 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const eventsData = useLoaderData();
+
+  // if (eventsData.isError) {
+  //   return <p>{eventsData.message}</p>;
+  // }
+
   const events = eventsData.events;
 
-  return <EventsList events={eventsData} />;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
@@ -15,6 +20,8 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    // return { isError: true, message: "Failed to fetch events" };
+    throw { message: "Failed to fetch events" };
   } else {
     return response;
   }
