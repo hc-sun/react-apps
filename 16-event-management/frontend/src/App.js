22145d6296b039438c47ccbff1e7302492a54b8a
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-02-20 18:34:03 +0800

    use action function to handle form submission

--- 16-event-management/frontend/src/App.js
+++ 16-event-management/frontend/src/App.js
@@ -3,7 +3,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Events, { loader as eventsLoader } from "./pages/Events";
 import EventDetail, { loader as eventDetailLoader } from "./pages/EventDetail";
-import NewEvent from "./pages/NewEvent";
+import NewEvent, { action as newEventAction } from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRoot from "./pages/EventsRoot";
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEvent /> },
             ],
           },
-          { path: "new", element: <NewEvent /> },
+          { path: "new", element: <NewEvent />, action: newEventAction },
         ],
       },
     ],
--- 16-event-management/frontend/src/pages/NewEvent.js
+++ 16-event-management/frontend/src/pages/NewEvent.js
@@ -1,5 +1,34 @@
+import { json, redirect } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return <h1>New Event Page</h1>;
+  return <EventForm />;
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  // extract the data from the form
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Failed to save event" }, { status: 500 });
+  }
+  return redirect("/events");
+}
