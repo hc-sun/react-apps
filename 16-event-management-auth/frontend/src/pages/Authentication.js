91d7407525f88f05588594d25440570a180c076d
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-03-01 18:49:51 +0100

    implemented authAction

--- 16-event-management-auth/frontend/src/pages/Authentication.js
+++ 16-event-management-auth/frontend/src/pages/Authentication.js
@@ -1,7 +1,42 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Invalid mode" }, 400);
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: "POST",
+    body: JSON.stringify(authData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Authentication failed" }, 500);
+  }
+
+  //after login redirect to home page
+  return redirect("/");
+}
