6c7a4b28e0b5b8f6b99da51dc95dbcb361bee62d
hc-sun <34866479+hc-sun@users.noreply.github.com>
2024-03-06 19:06:03 +0100

    calculate created token duration for expiration

--- 16-event-management-auth/frontend/src/util/auth.js
+++ 16-event-management-auth/frontend/src/util/auth.js
@@ -1,24 +1,25 @@
-import { redirect } from 'react-router-dom';
+import { redirect } from "react-router-dom";
 
 export function getTokenDuration() {
-  const storedExpirationDate = localStorage.getItem('expiration');
-  const expirationDate = new Date(storedExpirationDate);
+  const storedExpireDate = localStorage.getItem("expires");
+  const expiresDate = new Date(storedExpireDate);
   const now = new Date();
-  const duration = expirationDate.getTime() - now.getTime();
+  const duration = expiresDate.getTime() - now.getTime();
   return duration;
 }
 
 export function getAuthToken() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
+  // if there is no token, no need to check the expiration
   if (!token) {
     return null;
   }
 
   const tokenDuration = getTokenDuration();
 
-  if (tokenDuration < 0) {
-    return 'EXPIRED';
+  if (tokenDuration <= 0) {
+    return "EXPIRED";
   }
 
   return token;
@@ -33,6 +34,6 @@ export function checkAuthLoader() {
   const token = getAuthToken();
 
   if (!token) {
-    return redirect('/auth');
+    return redirect("/auth");
   }
 }
